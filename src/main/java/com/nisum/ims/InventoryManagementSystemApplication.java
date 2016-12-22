4a62916e3972f5b604c83c12342c96b38c95cb52
nisum <nisum@10.0.0.35>
2016-12-22 20:21:03 +0530

    Google Signin

--- src/main/java/com/nisum/ims/InventoryManagementSystemApplication.java
+++ src/main/java/com/nisum/ims/InventoryManagementSystemApplication.java
@@ -1,12 +1,19 @@
-package com.ims;
+package com.nisum.ims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@Controller
+@RefreshScope
 public class InventoryManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementSystemApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/nisum/ims/controller/GoogleSignInController.java
@@ -0,0 +1,25 @@
+package com.nisum.ims.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class GoogleSignInController {
+
+	@GetMapping()
+	public String welcomePage() {
+		return "redirect:index.html";
+	}
+	@GetMapping
+	@RequestMapping("/OAuth2Callback")
+	public String callback(@RequestParam String code) {
+		if (StringUtils.isNotBlank(code)) {
+			return "redirect:dashboard.html";
+		}
+		return "redirecting code value is not authenticated";
+	}
+}
